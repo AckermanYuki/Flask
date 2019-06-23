d43de4f5be55fd44fc0c0d75fafb4e640a2d91c4
AckermanYuki <danielgsn99@gmail.com>
2019-06-23 00:11:46 -0300

    Resolução de resposividade, verificação de usuários e finalização da tela de cadastro

--- app/controllers/defaut.py
+++ app/controllers/defaut.py
@@ -24,26 +24,62 @@ def login():
             login_user(user)
             return redirect(url_for("index"))
         else:
-            flash("Login invalido, Tente novamente.")
+            return redirect(url_for("loginalert", form=form))
     return render_template('login.html', form=form)
+
+@app.route("/loginalert", methods=["GET","POST"])
+def loginalert():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user and user.password == form.password.data:
+            login_user(user)
+            return redirect(url_for("index"))
+        else:
+            return render_template('loginalert.html', form=form)
+    return render_template('loginalert.html', form=form)
 
 @app.route("/cadastro", methods=["GET","POST"])
 def cadastro():
-    if request.method == "POST":
-        username = request.form.get("username")
-        password = request.form.get("senha")
-        name = request.form.get("name")
-        email = request.form.get("email")
-        cpf = request.form.get("cpf")
-        telefone = request.form.get("telefone")
-        
-        if username and password and name and email and cpf and telefone:
-            cad = User(username, password, name, email, cpf, telefone)
-            db.session.add(cad)
-            db.session.commit()  
-        return redirect(url_for("login"))
-    else:
-        return render_template('cadastro.html')
+    try:
+        if request.method == "POST":
+            username = request.form.get("username")
+            password = request.form.get("senha")
+            name = request.form.get("name")
+            email = request.form.get("email")
+            cpf = request.form.get("cpf")
+            telefone = request.form.get("telefone")
+            
+            if username and password and name and email and cpf and telefone:
+                cad = User(username, password, name, email, cpf, telefone)
+                db.session.add(cad)
+                db.session.commit()  
+            return redirect(url_for("login"))
+        else:
+            return render_template('cadastro.html')
+    except:
+        return redirect(url_for("cadastroalert"))
+
+@app.route("/cadastroalert", methods=["GET","POST"])
+def cadastroalert():
+    try:
+        if request.method == "POST":
+            username = request.form.get("username")
+            password = request.form.get("senha")
+            name = request.form.get("name")
+            email = request.form.get("email")
+            cpf = request.form.get("cpf")
+            telefone = request.form.get("telefone")
+            
+            if username and password and name and email and cpf and telefone:
+                cad = User(username, password, name, email, cpf, telefone)
+                db.session.add(cad)
+                db.session.commit()  
+            return redirect(url_for("login"))
+        else:
+            return redirect(url_for("cadastro"))
+    except:
+        return render_template('cadastroalert.html')
 
 @app.route("/logout")
 def logout():
